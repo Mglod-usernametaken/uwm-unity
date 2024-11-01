78290dd3e6791a35cbeae03242aeff541bb06d03
Walord Titan <masterwloczykij@gmail.com>
2024-11-01 16:00:41 +0100

    add l3z3

--- Assets/lab3z3.cs
+++ Assets/lab3z3.cs
@@ -6,56 +6,23 @@ public class lab3z1 : MonoBehaviour
 {
     public float speed = 2.0f;
     private Vector3 startPosition;
-    private int rotationCount = 0;
-    private float side = 10.0f;
+    private Vector3 direction = Vector3.right; // Inicjalizuj kierunek
+    private float distance = 10.0f;
 
-    // Start is called before the first frame update
     void Start()
     {
         startPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        switch(currentSide)
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if (Vector3.Distance(startPosition, transform.position) >= distance)
         {
-            case 0:
-                transform.Translate(Vector3.right * speed * Time.deltaTime);
-                if (transform.position.x >= startPosition.x + sideLength) 
-                {
-                    transform.Rotate(0,90,0);
-                    rotationCount++;
-                }
-                break;
-
-            case 1:
-                transform.Translate(Vector3.forward * speed * Time.deltaTime);
-                if (transform.position.z >= startPosition.z + sideLength) 
-                {
-                    transform.Rotate(0,90,0);
-                    rotationCount++;
-                }
-                break;
-
-            case 2:
-                transform.Translate(Vector3.left * speed * Time.deltaTime);
-                if (transform.position.x <= startPosition.x) 
-                {
-                    transform.Rotate(0,90,0);
-                    rotationCount++;
-                }
-                break;
-
-            case 3:
-                transform.Translate(Vector3.back * speed * Time.deltaTime);
-                if (transform.position.z >= startPosition.z)
-                {
-                    transform.Rotate(0,90,0);
-                    rotationCount++;
-                }
-                break;
-
-    }
+            direction = Quaternion.Euler(0, 90, 0) * direction; // Zmiana kierunku o 90 stopni
+            startPosition = transform.position; // Ustaw nową pozycję startową
+        }
     }
 }
+
