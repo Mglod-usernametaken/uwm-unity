0fd6b44c7ca77733140c921a4707d542708c4db4
Walord Titan <masterwloczykij@gmail.com>
2024-11-02 13:14:47 +0100

    add lab3z5

---
+++ Assets/prefab_script.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prefab_script: MonoBehaviour
+{
+    public GameObject myPrefab;
+    void Start()
+    {
+        Instantiate(myPrefab, new Vector3(0,0,0), Quaternion.identity);
+    }
+
+    void Update()
+    {
+        
+    }
+}
